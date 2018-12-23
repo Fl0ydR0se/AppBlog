5d80b50a22cf425b4c9e20c3a83268b5d18d954c
Fl0ydR0se <lazariev.oleg@gmail.com>
2018-12-23 15:04:12 +0200

    Create the simple record test

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect
 
 const APP = "https://myappbloggg.herokuapp.com/";
 
-const lead = {
+const randomWord = {
     name: faker.name.firstName(),
     email: faker.internet.email(),
     phone: faker.phone.phoneNumber(),
@@ -19,7 +19,9 @@ browser;
 
 beforeAll(async () => {
     browser = await puppeteer.launch({
-      headless: true});
+      headless: true,
+      //slowMo: 100
+    });
 
     page = await browser.newPage();
   });
@@ -29,22 +31,16 @@ beforeAll(async () => {
   });
 
 
-  describe.skip("Contact form", () => {
-    test("lead can submit a contact request", async () => {
+  describe("Create the", () => {
+    test("simple record", async () => {
       await page.goto(APP);
-      await page.waitForSelector("[data-test=contact-form]");
-      await page.click("input[name=name]");
-      await page.type("input[name=name]", lead.name);
-      await page.click("input[name=email]");
-      await page.type("input[name=email]", lead.email);
-      await page.click("input[name=tel]");
-      await page.type("input[name=tel]", lead.phone);
-      await page.click("textarea[name=message]");
-      await page.type("textarea[name=message]", lead.message);
-      await page.click("input[type=checkbox]");
-      await page.click("button[type=submit]");
-      await page.waitForSelector(".modal");
-    });
+      await page.waitForSelector(".card-content");
+      await page.click(".btn-floating");
+      await page.waitForSelector(".modal-content");
+      await page.type("#title", randomWord.name);
+      await page.type("#text", randomWord.message);
+      await page.waitForSelector("#createPost");
+    },90000);
   });
 
   describe("Testing the title", () => {
@@ -52,5 +48,5 @@ beforeAll(async () => {
       await page.goto(APP);
       let title = await page.title();
       expect(title).to.equal("Main page | My simple blog");      
-    });
-  },900000);
+    },90000);
+  });
